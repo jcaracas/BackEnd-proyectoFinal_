521b54cce5bd45cb61b2fd096ce144d2160441cb
jose caracas <josecaracas@gmail.com>
2023-04-06 19:38:43 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,191 @@
+const express = require('express')
+const jwt = require("jsonwebtoken")
+const app = express()
+const cors = require('cors')
+const { reporteConsultas} = require("./middlewares/miders")
+
+app.listen(3000, console.log("¡Servidor encendido!"))
+app.use(cors())
+app.use(express.json())
+
+const params = "paralelepipedo123"
+
+const { verificarCredenciales, registrarUsuario, generarPerfiles, obtenerUsuarios, updateUsuarios, registrarProductos, registrarCategorias, obtenerProductos, obtenerProductosId, comprar, obtenerCompras, obtenerUsuario, modificarUsuario } = require('./middleware')
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body
+    await verificarCredenciales(email, password)
+    // se utiliza middleware jwt
+    const token = jwt.sign({ email }, params)
+    res.status(200).send(token)
+  } catch (error) {
+    console.log(error)
+    res.status(error.code || 500).send(error)
+  }
+}
+)
+// crear usuarios
+app.post("/usuarios", reporteConsultas,async (req, res) => {
+  try {
+    const usuario = req.body
+    await registrarUsuario(usuario)
+    res.status(200).send("Usuario creado con éxito")
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+)
+//select usuarios
+app.get("/usuarios", reporteConsultas,async (req, res) => {
+  try {
+    const Authorization = req.header("Authorization")
+    const token = Authorization.split("Bearer ")[1]
+    jwt.verify(token, params)
+    const rows = await obtenerUsuarios()
+    res.status(400).send(rows)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+)
+
+
+//update usuarios
+app.put("/usuarios/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const Authorization = req.header("Authorization")
+    const token = Authorization.split("Bearer ")[1]
+    jwt.verify(token, params)
+    const rows = await updateUsuarios(id, req.body)
+    res.send(rows)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+)
+
+app.post("/perfiles", async (req, res) => {
+  try {
+    const perfil = req.body
+    const Authorization = req.header("Authorization")
+    const token = Authorization.split("Bearer ")[1]
+    jwt.verify(token, params)
+    await generarPerfiles(perfil)
+    res.send("Perfil creado con éxito")
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+)
+// crear productos
+app.post("/productos", async (req, res) => {
+  try {
+    const producto = req.body
+    const Authorization = req.header("Authorization")
+    const token = Authorization.split("Bearer ")[1]
+    jwt.verify(token, params)
+    await registrarProductos(producto)
+    res.send("Producto creado con éxito")
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+)
+// crear categorias
+app.post("/categoria", async (req, res) => {
+  try {
+    const categoria = req.body
+    const Authorization = req.header("Authorization")
+    const token = Authorization.split("Bearer ")[1]
+    jwt.verify(token, params)
+    await registrarCategorias(categoria)
+    res.send("Categoria creado con éxito")
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+)
+
+//select productos
+app.get("/productos", async (req, res) => {
+  try {
+    const rows = await obtenerProductos()
+    console.log(rows)
+    res.send(rows)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+)
+
+//select productos detalle
+app.get("/productos/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const rows = await obtenerProductosId(id)
+    res.send(rows)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+)
+
+app.post("/comprar", async (req, res) => {
+  try {
+    const Authorization = req.header("Authorization")
+    const datosCompra = req.body
+    const token = Authorization.split("Bearer ")[1]
+    jwt.verify(token, params)
+    const { email } = jwt.decode(token)
+    await comprar(email, datosCompra)
+    if (comprar) res.status(200).send(`Compra realizada`)
+    else res.status(404).send({ message: "No se encontró ninguna compra" })
+  } catch (error) {
+    res.status(500).send(error)
+    console.log(error);
+  }
+})
+//select usuarios
+app.get("/compras", async (req, res) => {
+  try {
+    const Authorization = req.header("Authorization")
+    const token = Authorization.split("Bearer ")[1]
+    jwt.verify(token, params)
+    const { email } = jwt.decode(token)
+    const rows = await obtenerCompras(email)
+    res.status(400).send(rows)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+)
+
+
+// Agregar esta ruta para obtener el usuario logueado
+app.get("/usuario", reporteConsultas,async (req, res) => {
+  try {
+    const Authorization = req.header("Authorization")
+    const token = Authorization.split("Bearer ")[1]
+    jwt.verify(token, params)
+    const { email } = jwt.decode(token)
+    const usuario = await obtenerUsuario(email)
+    //res.json(usuario)
+    res.send(usuario)
+  } catch (error) {
+    res.status(500).send(error)
+    console.log(error);
+  }
+
+
+})
+//Ruta para editar el usuario que se obtiene en la ruta anterior
+app.put("/usuario/:id", reporteConsultas,async (req, res) => {
+  const { id } = req.params
+  const usuario = req.body
+  await modificarUsuario(usuario, id)
+  res.send("Usuario modificado con éxito")
+})
+
+module.exports = app
---
+++ middleware.js
@@ -0,0 +1,139 @@
+const pool = require('./conexion')
+const bcrypt = require('bcryptjs')
+
+const verificarCredenciales = async (email, password) => {
+  const values = [email]
+  const consulta = "SELECT * FROM usuarios WHERE email = $1"
+  const { rows: [usuario], rowCount } = await pool.query(consulta, values)
+  if (rowCount == 0) {
+    throw { code: 401, message: "Email o contraseña incorrecta" }
+  } else {
+    const { password: passwordEncriptada } = usuario
+    const passwordEsCorrecta = bcrypt.compareSync(password, passwordEncriptada)
+  }
+}
+
+const registrarUsuario = async (usuario) => {
+  let { rut, email, nombre, password } = usuario
+  const pwBcrypt = bcrypt.hashSync(password)
+  passwd = pwBcrypt
+  const values = [rut, email, nombre, passwd]
+  console.log(values);
+  const consulta = "INSERT INTO usuarios (id,rut,email,nombre,password) values (DEFAULT, $1, $2, $3, $4)"
+  await pool.query(consulta, values)
+}
+
+const generarPerfiles = async (perfil) => {
+  let { nombre, descripcion } = perfil
+  const values = [nombre, descripcion]
+  const consulta = "INSERT INTO perfiles (id,nombre,descripcion) values (DEFAULT,$1,$2)"
+  await pool.query(consulta, values)
+}
+
+const obtenerUsuarios = async () => {
+  const { rows } = await pool.query("SELECT email FROM usuarios")
+  console.log(rows)
+  return rows
+}
+const updateUsuarios = async (id, body) => {
+  const { rut, email, nombre, direccion, telefono, img, status } = body
+  const perfil = { rut, email, nombre, direccion, telefono, img, status }
+  const result = await pool.query("UPDATE usuarios SET ? WHERE id = ?", [perfil, id])
+  res.json(result)
+}
+/** productos* */
+const registrarProductos = async (producto) => {
+  let { titulo, usuario_id, precio, descripcion, stock, categoria_id, imagen1 } = producto
+  const values = [usuario_id, titulo, precio, descripcion, stock, categoria_id, imagen1]
+  const consulta = "INSERT INTO publicaciones (id,usuario_id,titulo,precio,descripcion,stock,categoria_id,imagen1) values (DEFAULT,$1,$2,$3,$4,$5,$6,$7)"
+  await pool.query(consulta, values)
+}
+/***categorias */
+const registrarCategorias = async (cat) => {
+  let { categoria, descripcion } = cat
+  const values = [categoria, descripcion]
+  const consulta = "INSERT INTO categorias (id,categoria,descripcion) values (DEFAULT,$1,$2)"
+  await pool.query(consulta, values)
+}
+
+const obtenerProductos = async () => {
+  const consulta = "SELECT * FROM publicaciones"
+  const { rows } = await pool.query(consulta)
+  return rows
+}
+
+const obtenerProductosId = async (id) => {
+  let values = [id]
+  const consulta = "SELECT * FROM publicaciones WHERE id = $1"
+  const { rows } = await pool.query(consulta, values)
+  return rows
+}
+
+const insertarDetalle = async (publicacion_id, cantidad, precio, item, codigo) => {
+  const values = [publicacion_id, cantidad, precio, item, parseInt(codigo)]
+  console.log(values)
+  const queryIn = 'INSERT INTO "detalleOrden" (id, publicacion_id, cantidad, precio, item, orden_id) VALUES (DEFAULT, $1, $2, $3, $4, $5)'
+  await pool.query(queryIn, values)
+  const updateStock = "UPDATE publicaciones SET stock=stock-$2 WHERE id=$1"
+  const value = [publicacion_id, cantidad]
+  await pool.query(updateStock, value)
+
+}
+const comprar = async (email, datosCompra) => {
+  const value = [email]
+  const query = "select id,concat(extract(month from current_date),extract(day from current_date),id, trunc(random() * 99 + 13)) as codigo from usuarios where email = $1"
+  const usuario_id = await pool.query(query, value)
+  let id = usuario_id.rows[0].id
+  let orden_id = usuario_id.rows[0].codigo
+  let item = 1
+  let total = 0
+
+  datosCompra.map((e) => {
+    insertarDetalle(e.id, 1, e.price, item, orden_id)
+    item++;
+    total += (e.price * 1)
+  });
+
+  const values = [id, total, datosCompra, orden_id]
+  const InsertOrden = "INSERT INTO ordenes (id, usuario_id, total, observacion,orden_id,estado_id) VALUES (DEFAULT, $1 ,$2, $3, $4, 1)"
+  await pool.query(InsertOrden, values)
+
+  console.log("Compra agregada con exito");
+}
+const obtenerCompras = async (email) => {
+  const value = [email]
+  const query = "select id from usuarios where email = $1"
+  const usuario_id = await pool.query(query, value)
+  let id = usuario_id.rows[0].id
+  let values = [id]
+  const consulta = `SELECT c.id,b.titulo,b.precio,b.imagen1
+                        FROM ordenes AS c
+                        LEFT JOIN "detalleOrden" a
+                        ON a.orden_id = c.orden_id
+                        LEFT JOIN publicaciones b
+                        ON b.id = a.publicacion_id
+                      WHERE c.usuario_id = $1
+                        AND b.titulo <> ''
+                        ORDER BY c.id ASC`
+  const { rows } = await pool.query(consulta, values)
+  return rows
+}
+// agregar esta funcion que obtiene los datos del usuario logueado
+const obtenerUsuario = async (email) => {
+  const values = [email]
+  const consulta = "select * from usuarios where email=$1"
+  const { rows } = await pool.query(consulta, values)
+  console.log("datos entregados: ", rows);
+  return rows
+}
+
+// agregar funcion para modificar el usuario
+const modificarUsuario = async (usuario, id) => {
+  let { nombre, direccion, telefono, password } = usuario
+  const pwBcrypt = bcrypt.hashSync(password)
+  passwd = pwBcrypt
+  const values = [id, nombre, direccion, telefono, passwd]
+  const consulta = "UPDATE usuarios	SET nombre=$2, direccion=$3, telefono=$4, password=$5 WHERE id=$1"
+  const result = await pool.query(consulta, values)
+}
+module.exports = { verificarCredenciales, registrarUsuario, generarPerfiles, obtenerUsuarios, updateUsuarios, registrarProductos, registrarCategorias, obtenerProductos, obtenerProductosId, comprar, obtenerCompras, obtenerUsuario, modificarUsuario }
